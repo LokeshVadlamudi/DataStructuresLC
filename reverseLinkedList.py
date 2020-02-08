1d692dd0cba4566a98081a03471d5c322eeb9690
Lokesh Vadlamudi <lokesh.dev96@gmail.com>
2020-02-08 00:05:39 -0800

    better approach

--- reverseLinkedList.py
+++ reverseLinkedList.py
@@ -1,12 +1,40 @@
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        p1, p2 = None, head
-        while p2 != None:
-            p3 = p2.next
-            p2.next = p1
-            p1 = p2
-            p2 = p3
-        return p1
+
+        if headA == None or headB == None:
+            return None
+        ap = headA
+        bp = headB
+
+        acount = 0
+        bcount = 0
+
+        while (True):
+
+            if ap == bp:
+                return ap
+            if ap != None:
+                ap = ap.next
+            else:
+                ap = headB
+                acount += 1
+            if bp != None:
+                bp = bp.next
+            else:
+                bp = headA
+                bcount += 1
+
+            if acount == 2 and bcount == 2:
+                return None
+
+
+        # p1, p2 = None, head
+        # while p2 != None:
+        #     p3 = p2.next
+        #     p2.next = p1
+        #     p1 = p2
+        #     p2 = p3
+        # return p1
 
 
 
