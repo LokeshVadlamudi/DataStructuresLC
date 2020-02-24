9d7801edb889472249e04d37f973972a2b1b7d52
Lokesh Vadlamudi <lokesh.dev96@gmail.com>
2020-02-23 20:29:03 -0800

    Day 17

---
+++ Day 17.py
@@ -0,0 +1,28 @@
+# 1361. Validate Binary Tree Nodes
+# n = 4
+# leftChild = [1, -1, 3, -1]
+# rightChild = [2, -1, -1, -1]
+#
+# children = 0
+# for i in range(n):
+#     if leftChild[i] != -1:
+#         children += 1
+#     if rightChild[i] != -1:
+#         children += 1
+# print(children == n-1)
+
+
+# # 1362. Closest Divisors
+# num = 8
+# import math
+# for a in range(math.isqrt(num+2),0,-1):
+#     for y in num+1,num+2:
+#         if y % a == 0:
+#             print([a,y//a])
+
+
+
+
+
+
+
