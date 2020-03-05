738f09806b7c7f2d4b6c2b6ee38f3da6a92165a2
Lokesh Vadlamudi <lokesh.dev96@gmail.com>
2020-03-04 20:08:26 -0800

    Create Day27.py

---
+++ Day27.py
@@ -0,0 +1,61 @@
+# 151. Reverse Words in a String
+
+# s = "the sky is blue"
+# res = s.split(' ')
+# ans = []
+# for i in res:
+#     if i != '':
+#         ans.append(i)
+# s = ""
+# for i in ans:
+#     s = i + " " + s
+#
+# print(s[:-1])
+
+
+
+
+
+
+# 1130. Minimum Cost Tree From Leaf Values
+
+# arr = [6,2,4]
+# #
+# # ans = 0
+# #
+# # while len(arr) > 1:
+# #     i = arr.index(min(arr))
+# #     print(arr[i-1:i]+arr[i+1:i+2])
+# #     ans += min(arr[i-1:i]+arr[i+1:i+2])*arr.pop(i)
+# # print(ans)
+
+# 53. Maximum Subarray
+# nums = [-2,1,-3,4,-1,2,1,-5,4]
+#
+# for i in range(1,len(nums)):
+#     nums[i] = max(nums[i],nums[i]+nums[i-1])
+#
+# print(max(nums))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
